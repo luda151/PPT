20304eaacff02e0edf8fa7745cc3ddc5bde07852
Dagmar Stranska <stranskad@vlp.cz>
2019-12-19 19:29:10 +0100

    fix

--- Fibon/src/fibon/Fibon.java
+++ Fibon/src/fibon/Fibon.java
@@ -6,8 +6,8 @@ public class Fibon {
 		int previousNumber = 0;
 		int nextNumber = 1;
 
-		System.out.print("Fibonacciho posloupnost "+maxNumber+" ÄÃ­sel\n ");
-		System.out.print("VÃ½poÄet smyÄkou\n");
+		System.out.print("Fibonacciho posloupnost "+maxNumber+" cisell\n ");
+		System.out.print("Vypocet smyckou\n");
 
 		for (int i = 1; i <= maxNumber; ++i)
 		{
@@ -18,7 +18,7 @@ public class Fibon {
 			nextNumber = sum;
 		}
 		System.out.println();
-		System.out.print("VÃ½poÄet rekurzÃ­\n");
+		System.out.print("Vypocet pomoci rekurze\n");
 
 		FibonacciCalc fibRek = new FibonacciCalc();
 
@@ -28,34 +28,15 @@ public class Fibon {
 
 
 		System.out.println();
-		System.out.println("VÃ½poÄet pomocÃ­ dynamickÃ©ho programovÃ¡nÃ­");
+		System.out.println("Vypocet pomoci­ dynamickeho programova¡ni­");
 
-		FibonTable finbTable = new FibonTable(10);
+		FibonTable finbTable = new FibonTable(maxNumber);
 
-
-		//System.out.println(finbTable.fibDP(8));
-
-
-		//finbTable.FibonTable(maxNumber);
-		int previousNumber = 0;
-		int nextNumber = 1;
-
-		System.out.print("Fibonacciho posloupnost "+maxNumber+" èísel: ");
-
-		for (int i = 1; i <= maxNumber; ++i)
-		{
-			System.out.print(previousNumber+" ");
-
-
-
-			int sum = previousNumber + nextNumber;
-			previousNumber = nextNumber;
-			nextNumber = sum;
-		}
 
 	}
 
-} // Java code Loop
+} 
+
 
 
 
--- Fibon/src/fibon/FibonacciCalc.java
+++ Fibon/src/fibon/FibonacciCalc.java
@@ -1,37 +1,15 @@
 package fibon;
-<<<<<<< HEAD
 
-public class FibonacciCalc {
-
-	public FibonacciCalc() {}
-
-	public int fibonacciRecursion(int n){
-
-=======
-// užití rekurze
 public class FibonacciCalc {
 	public static int fibonacciRecursion(int n){
->>>>>>> 8e522422fd4c5d9e14833050458b4259443808e8
+
 		if(n == 0){
 			return 0;
 		}
 		if(n == 1 || n == 2){
-<<<<<<< HEAD
+
 			return 1;
 		}
 		return fibonacciRecursion(n-2) + fibonacciRecursion(n-1);
 	}
-=======
-				return 1;
-			}
-		return fibonacciRecursion(n-2) + fibonacciRecursion(n-1);
-		}
-	    public static void main(String args[]) {
-		int maxNumber = 10;
-		System.out.print("Fibonacciho posloupnost "+maxNumber+" èísel: ");
-		for(int i = 0; i < maxNumber; i++){
-				System.out.print(fibonacciRecursion(i) +" ");
-			}
-		}
->>>>>>> 8e522422fd4c5d9e14833050458b4259443808e8
 }
