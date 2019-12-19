6a8400d68c371277e9d0077019e228935254a344
Dagmar Stranska <stranskad@vlp.cz>
2019-12-19 19:40:03 +0100

    add comments

--- KvadrRo/src/Main.java
+++ KvadrRo/src/Main.java
@@ -1,95 +1,28 @@
-import java.util.Arrays;
-import java.util.Scanner;
-public class Main {
+//Fibonacci Series using Dynamic Programming 
+#include<stdio.h> 
+  
+int fib(int n) 
+{ 
+  / Declare an array to store Fibonacci numbers. /
+  int f[n+2];   // 1 extra to handle case, n = 0 
+  int i; 
+  
+  /* 0th and 1st number of the series are 0 and 1*/
+  f[0] = 0; 
+  f[1] = 1; 
+  
+  for (i = 2; i <= n; i++) 
+  { 
 
-	public static void main(String[] args) {
-
-		/*
-		Scanner scanner;
-		scanner = new Scanner(System.in);
-		System.out.println("Kvadratická rovnice ax^2 + bx + c = 0");
-		System.out.print("Zadej a: ");
-		double a= scanner.nextdouble();
-		System.out.print("Zadej b: ");
-		double b= scanner.nextdouble();
-		System.out.print("Zadej c: ");
-		double c= scanner.nextdouble();
-		*/
-		
-		double a=7;
-		double b=4;
-		double c=-3;
-		Tool_IO io = new Tool_IO();
-		KvadratickaFunkce kf = new KvadratickaFunkce(a,b,c); //volani konstruktoru
-		kf.vypocetKorenu(); 
-
-		if(kf.diskriminant() > 0) {
-			
-//			System.out.println("Funkcční hodnota rovnice");
-//			System.out.print("Zadej x: ");
-			//double x= scanner.nextdouble();
-			double x = 0;
-			double funHodnota=kf.vypocetFunkciHodnoty(x);
-			
-//			System.out.println("Funkční hodnota v bodě x= "+x+" je: "+funHodnota);
-			io.writeToFile("Funkční hodnota v bodě x= "+x+" je: "+funHodnota);
-			io.writeNewLine();
-
-		}
-		
-		System.out.println();
-//		System.out.println("Vzorkovani");
-		io.writeToFile("Vzorkovani");
-		io.writeNewLine();
-		
-		/*
-		System.out.print("Zadej Minimum: ");
-		double min= scanner.nextdouble();
-		System.out.print("Zadej Maximum: ");
-		double max= scanner.nextdouble();
-		System.out.print("Zadej 'Krok': ");
-		double step= scanner.nextdouble();
-		*/
-		double min= -6;
-		double max= 6;
-		double step = 0.5;
-
-		Tool_Funkce toolFce = new Tool_Funkce(min,max,step,kf); //volani kontruktoru
-		
-		//-------------------------------------------------------------------------------------
-		io.writeToFile("-------------------------\n");
-		io.writeToFile("Vzorkovani_I\n");
-//		System.out.println("-------------------------");
-//		System.out.println("Vzorkovani_I");
-		/* Vzorkovani_I */
-		// Nevyhody
-		// -  nevime kolik bude vysledku, takze nevime jak velke pole priparavit. 
-		double vysledky[] = toolFce.vzorkovani_I();	
-//		System.out.println("Vysledky:\n"+ Arrays.toString(vysledky));
-		io.writeToFile("Vysledky:\n"+ Arrays.toString(vysledky));
-		io.writeNewLine();
-		io.writeNewLine();
-		
-//		System.out.println("Vzorkovani_II");
-		io.writeToFile("Vzorkovani_II\n");
-		/* Vzorkovani_II */
-		// Nevyhody
-		// - nevime kolik bude vysledku, takze nevime jak velke pole priparavit. 
-		// - je obtizne vracet dve pole, proto vypisujeme vysledky jiz ve volani funkce
-		 toolFce.vzorkovani_II(io);	
-
-//		System.out.println("Vzorkovani_III");
-		io.writeToFile("Vzorkovani_III\n");
-		/* Vzorkovani_III */
-		// Nevyhody
-		// - nevime kolik bude vysledku, takze nevime jak velke pole priparavit. 
-		Vzorek vysledky_III[]=toolFce.vzorkovani_III();	
-
-//		System.out.println("Vysledky:\n"+ Arrays.toString(vysledky_III));
-		io.writeToFile("Vysledky:\n"+ Arrays.toString(vysledky_III));
-		io.writeNewLine();
-//		scanner.close();
-
-		io.read();
-	}
-}
+      f[i] = f[i-1] + f[i-2]; 
+  } 
+  
+  return f[n]; 
+} 
+  
+int main () 
+{ 
+  int n = 9; 
+  printf("%d", fib(n)); 
+  getchar(); 
+  return 0; 
